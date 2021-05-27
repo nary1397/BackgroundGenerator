70d4639ee2d40792eb7f7356705916750ff17d0f
nary1397 <84886912+nary1397@users.noreply.github.com>
2021-05-27 11:21:43 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var body = document.getElementById('gradient'); // 바디 태그
+var color1 = document.querySelector('.color1'); // 컬러 입력창1
+var color2 = document.querySelector('.color2'); // 컬러 입력창2
+var h3 = document.querySelector('h3');          // 빈태그 h3
+
+// 이벤트 연결
+color1.addEventListener('input', setGrid);
+color2.addEventListener('input', setGrid);
+
+function setGrid() {
+    body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")"
+    h3.textContent = body.style.background;
+}
+
+
